2c656b996359e48bc62b674f50425788d7af6172
김원배 <k1bae2301@gmail.com>
2019-02-19 20:49:04 +0900

    SWEA_1210_Ladder1_D4

--- SWEA/SWEA_1210.cpp
+++ SWEA/SWEA_1210.cpp
@@ -4,14 +4,14 @@ using namespace std;
 int main()
 {
     int T = 0;
-    cin>>T;
-    
-    for(int tc = 0; tc < 10; tc++){
-        int data[100][100] = {0,};
-        int x=0,y=0;
+    int data[101][101] = {0,};
 
+    for(int tc = 0; tc < 10; tc++){
+        cin>>T;
+        int x,y;
+        
         //초기화
-        //memset(data,0,sizeof(data));
+        memset(data,0,sizeof(data));
 
         //test_case 받기
         for(int i = 0; i < 100; i++)
@@ -27,30 +27,24 @@ int main()
             
         }
 
-        // for(int i = 0; i < 100; i++)
-        // {
-        //     for(int j = 0; j < 100; j++)
-        //     {
-        //         cout<<data[i][j];
-        //     }
-        //     cout<<endl;
-        // }
-
+        //탐색
         while(x >= 0)
         {
             x--;
+            //왼쪽
             if(data[x][y-1] == 1){
                 do{
                     y--;
                 }while(data[x][y - 1] == 1);
             }
+            //오른쪽
             else if(data[x][y+1] == 1){
                 do{
                     y++;
                 }while(data[x][y + 1] == 1);
             }
         }
-        cout<<"#"<<T<<" "<<y+1<<endl;
+        cout<<"#"<<T<<" "<<y<<endl;
         
     }
     return 0;
