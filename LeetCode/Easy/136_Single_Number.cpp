03de6530c260a214eb7c783e42323c1b9e7f1537
김원배 <k1bae2301@gmail.com>
2019-03-27 01:42:27 +0900

    LeetCode Hashmap and XOR bit solution

---
+++ LeetCode/Easy/136_Single_Number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        //#1. Bit operation
+        int res = 0;
+        for(int i = 0; i < nums.size(); i++){
+            res ^= nums.at(i);
+        }
+        return res;
+        
+        // #2. Using HashMap
+        // unordered_set<int> s;
+        // for(int num : nums)
+        // {
+        //     if(s.count(num)) //최빈수, 끽해봐야 2
+        //         s.erase(num);
+        //     else
+        //         s.insert(num);
+        // }
+        // return *s.begin();
+    }
+};
