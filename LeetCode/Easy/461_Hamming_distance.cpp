41ecb052b77c8060dfd14a2647bc9c9a8c7d1489
김원배 <k1bae2301@gmail.com>
2019-03-26 21:32:09 +0900

    LeetCode is good

---
+++ LeetCode/Easy/461_Hamming_distance.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int res = x ^ y;
+        int count = 0;
+        
+        while(res){
+            if(res%2 == 1){
+                count++;
+            }
+            
+            res /= 2;
+        }
+        return count;
+    }
+};
