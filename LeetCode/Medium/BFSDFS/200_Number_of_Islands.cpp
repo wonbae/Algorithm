f3c16b1827ea848539ab4cfcbc4cfba535281694
김원배 <k1bae2301@gmail.com>
2019-04-05 00:46:48 +0900

    LeetCode Medium BFS

--- LeetCode/Medium/BFSDFS/200_Number_of_Islands.cpp
+++ LeetCode/Medium/BFSDFS/200_Number_of_Islands.cpp
@@ -1,23 +1,48 @@
-int m = grid.size(), n = m ? grid[0].size() : 0, islands = 0, offsets[] = {0, 1, 0, -1, 0};
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == '1') {
-                    islands++;
-                    grid[i][j] = '0';
-                    queue<pair<int, int>> todo;
-                    todo.push({i, j});
-                    while (!todo.empty()) {
-                        pair<int, int> p = todo.front();
-                        todo.pop();
-                        for (int k = 0; k < 4; k++) {
-                            int r = p.first + offsets[k], c = p.second + offsets[k + 1];
-                            if (r >= 0 && r < m && c >= 0 && c < n && grid[r][c] == '1') {
-                                grid[r][c] = '0';
-                                todo.push({r, c});
-                            }
-                        }
-                    }
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int row = grid.size() , col = grid[0].size();
+        int count = 0;
+        
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(grid[i][j] == 1){
+                    count++;
+                    BFS(i,j,grid);
                 }
             }
         }
-        return islands;
+        return count;
+        
+    }
+private:
+    void BFS(int x, int y, vector<vector<char>>& grid){
+        queue<pair<int,int>> q;
+        bool visited[grid.size()][grid.size()]; // 방문했는지를 표시하는 지도
+        int dx[] = {-1, 0, 1, 0};
+        int dy[] = {0, 1, 0, -1};
+        
+        q.push({x,y});
+        visited[x][y] = true;
+        
+        while(!q.empty()){
+            x = q.front().first;
+            y = q.front().second;
+            q.pop();
+            
+            for(int i = 0; i < 4; i++){
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                
+                if(nx >= 0 && nx < grid.size() && 0 <= ny && ny < grid[0].size()){
+                    if(visited[nx][ny] == false && grid[nx][ny] == 1){
+                        visited[nx][ny] = true;
+                        q.push({nx,ny});
+                    }
+                }
+            }
+            
+        }   
+    }
+    
+};
