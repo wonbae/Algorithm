3d6f24709f266885dea447101b8a54a85fde1b69
김원배 <wonbae@gim-wonbaeui-MacBookPro.local>
2019-08-20 06:14:15 +0900

    easy BOJ input/output

---
+++ BOJ/BOJ_10950/BOJ_10950.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int T,a,b;
+    cin>>T;
+
+    for(int tc = 0; tc < T; tc++){
+        cin>>a>>b;
+        cout<<a+b<<endl;
+    }
+
+
+    return 0;
+}
---
+++ BOJ/BOJ_10953/BOJ_10953.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int T,a,c;
+    char b;
+    cin>>T;
+    for(int i = 0; i < T; i++){
+        cin>>a>>b>>c;
+        cout<<a+c<<endl;
+    }
+    return 0;
+}
