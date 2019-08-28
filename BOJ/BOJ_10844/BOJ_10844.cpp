7a3605237d852c8da6aec3dbce23fb6951a8e6a1
wonbae kim <k1bae2301@gmail.com>
2019-08-29 01:31:00 +0900

    boj DP easy but not me

---
+++ BOJ/BOJ_10844/BOJ_10844.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+ 
+int mod = 1e9;
+int n, d[101][11];
+long long ans = 0;
+ 
+int main(void){
+    cin >> n;
+    for (int i = 1; i < 10; i++) d[1][i] = 1;
+    for (int i = 2; i <= n; i++){
+        for (int j = 0; j < 10; j++){
+            d[i][j] = (d[i - 1][j - 1] + d[i - 1][j + 1]) % mod;
+        }
+    }
+    for (int i = 0; i < 10; i++) ans += d[n][i];
+    cout << ans % mod << "\n";
+    return 0;
+}
