a829647911c1a05bd7970f5105a20c01887c7f75
wonbae kim <k1bae2301@gmail.com>
2019-08-31 07:43:42 +0900

    boj DP easy but not for me

---
+++ BOJ/BOJ_11057/BOJ_11057.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+#define mod 10007
+int n, d[1001][10];
+int ans = 0;
+ 
+int main(void){
+    cin >> n;
+    for (int i = 0; i < 10; i++) d[1][i] = 1;
+
+    for (int i = 2; i <= n; i++){
+        for (int j = 0; j < 10; j++){
+            for(int k = 0; k <= j; k++){
+                d[i][j] += d[i - 1][k];
+            }
+            d[i][j] %= mod;
+        }
+    }
+    for (int i = 0; i < 10; i++) ans += d[n][i];
+    cout << ans % mod << "\n";
+    return 0;
+}
