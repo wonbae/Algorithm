7256dde0b8b5e48428e5a72db54c41e496e339ff
wonbae kim <k1bae2301@gmail.com>
2019-10-04 23:32:35 +0900

    not solve have to change

---
+++ BOJ/BOJ_9251/BOJ_9251.cpp
@@ -0,0 +1,31 @@
+// // LCS(Longest Common Subsequence, 최장 공통 부분 수열)
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+
+
+
+//     return 0;
+// }
+
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+ 
+int main(){
+    string str[1000];
+    cin >> str[0];
+    int len = str[0].size();
+    
+    for(int i=1; i<len; i++){
+        str[i] = str[0].substr(i);
+        cout<< str[i] <<endl;
+    }
+    sort(str, str+len);
+    for(int i=0; i<len; i++)
+        cout << str[i] << endl;
+}
