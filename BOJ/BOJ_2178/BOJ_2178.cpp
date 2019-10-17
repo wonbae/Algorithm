01d39b9a6080f95f779b14f613a8beabf1de0be7
wonbae kim <k1bae2301@gmail.com>
2019-10-18 01:17:45 +0900

    BFS add another solution

--- BOJ/BOJ_2178/BOJ_2178.cpp
+++ BOJ/BOJ_2178/BOJ_2178.cpp
@@ -1,3 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX 100
+
+string Map[MAX];
+int dist[MAX][MAX] = {-1,};
+// int dist[MAX][MAX];
+int dx[] = {0,0,-1,1};
+int dy[] = {-1,1,0,0};
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    int count = 0;
+    int n,m;
+    cin>>n>>m;
+
+    // 입력
+    for(int i = 0; i < n; i++){
+        cin>>Map[i];
+    }
+    for(int i = 0; i < n; i++){
+        fill(dist[i],dist[i]+m,-1);
+    } 
+   
+    // // 출력 확인겸..
+    // for(int i = 0; i < n; i++){
+    //     for(int j = 0; j < m; j++){
+    //         cout<<Map[i][j];
+    //     }cout<<"\n";
+    // }
+
+    // for(int i = 0; i < n; i++){
+    //     for(int j = 0; j < m; j++){
+    //         cout<<dist[i][j];
+    //     }cout<<"\n";
+    // }
+
+    queue<pair<int, int> > q;
+
+    q.push(make_pair(0,0));
+    dist[0][0] = 0;
+
+    while(!q.empty()){
+        int x = q.front().first;
+        int y = q.front().second;
+
+        q.pop();
+
+        for(int idx = 0; idx < 4; idx++){
+            int nx = x + dx[idx];
+            int ny = y + dy[idx];
+
+            if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;    // 범위 벗어난다면 넘어감
+
+            if(dist[nx][ny] >= 0 || Map[nx][ny] != '1') continue;   // 방문한곳이고 갈수없는 0일 경우도 페스
+            
+            dist[nx][ny] = dist[x][y] + 1;
+            q.push(make_pair(nx,ny));
+            
+        }
+
+    }
+
+    // for(int i = 0; i < n; i++){
+    //     for(int j = 0; j < m; j++){
+    //         cout<<dist[i][j]<<", ";
+    //     }cout<<"\n";
+    // }
+
+    cout<<dist[n-1][m-1] + 1;
+
+    return 0;
+}
 
 
 
@@ -11,23 +85,7 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// 방법 2
 // #include <cstdio>
 // #include<iostream>
 // #include <queue>
