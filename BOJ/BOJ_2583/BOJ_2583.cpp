c8f4d71b71d0fa38397cc703ed65dd46979e7aec
wonbae kim <k1bae2301@gmail.com>
2019-12-23 21:30:30 +0900

    boj nothing

--- BOJ/BOJ_2583/BOJ_2583.cpp
+++ BOJ/BOJ_2583/BOJ_2583.cpp
@@ -6,34 +6,51 @@ using namespace std;
 int Map[MAX][MAX] = {0,};
 int visit[MAX][MAX] = {0,};
 int M, N;
+int K;
 
-void initmap(int lx, int ly, int rx, int ry){
-    // M is x axis, N is y axis;
-    int cnt = 0;
-    for(int y = ly; y < ry; y++){
-        for(int x = lx; x < rx; x++){
-            Map[y][x] = 1;
-        }
+
+// void initmap(int lx, int ly, int rx, int ry){
+//     // M is x axis, N is y axis;
+//     int cnt = 0;
+//     for(int y = ly; y < ry; y++){
+//         for(int x = lx; x < rx; x++){
+//             Map[y][x] = 1;
+//         }
+//     }
+// }
+
+void init() {
+    int x1, x2, y1, y2;
+
+    while(K--){
+        cin>>x1>>y1>>x2>>y2;
+
+        for(int i = x1; i < x2; i++)
+            for(int j = y1; j < y2; j++)
+                Map[j][i]=1;
     }
+    
 }
 
 int main(){
-    int K;
+    
 
-    cin>>N>>M>>K;
+    cin>>M>>N>>K;
 
-    for(int i = 0; i < K; i++){
-        int lx,ly,rx,ry;
-        cin>>lx>>ly>>rx>>ry;
-        initmap(lx, ly, rx, ry);
-    }
+    init();
+
+    // for(int i = 0; i < K; i++){
+    //     int lx,ly,rx,ry;
+    //     cin>>lx>>ly>>rx>>ry;
+    //     initmap(lx, ly, rx, ry);
+    // }
 
     for(int i = 0; i < N; i++){
-        for(int j = 0; j < M; j++){
-            cout<<Map[i][j]<< " ";
-        }
-        cout<<"\n";
+        for(int j = 0; j < M; j++)
+        {
+            cout<<Map[i][j];
+        }cout<<"\n";
     }
-
+            
     return 0;
 }
