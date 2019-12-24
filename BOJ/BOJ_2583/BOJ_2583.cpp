97468009c928f92fae0a07f50bbea78d6bc75c5f
wonbae kim <k1bae2301@gmail.com>
2019-12-24 15:42:16 +0900

    boj BFS

--- BOJ/BOJ_2583/BOJ_2583.cpp
+++ BOJ/BOJ_2583/BOJ_2583.cpp
@@ -7,49 +7,102 @@ int Map[MAX][MAX] = {0,};
 int visit[MAX][MAX] = {0,};
 int M, N;
 int K;
-
-
-// void initmap(int lx, int ly, int rx, int ry){
-//     // M is x axis, N is y axis;
-//     int cnt = 0;
-//     for(int y = ly; y < ry; y++){
-//         for(int x = lx; x < rx; x++){
-//             Map[y][x] = 1;
-//         }
-//     }
-// }
+vector<int> cnt;
 
 void init() {
-    int x1, x2, y1, y2;
+    int x1, y1, x2, y2;
 
     while(K--){
+        // (0,2), (4,4)
         cin>>x1>>y1>>x2>>y2;
-
-        for(int i = x1; i < x2; i++)
-            for(int j = y1; j < y2; j++)
-                Map[j][i]=1;
+        
+        for(int x = x1; x < x2; x++){
+            for(int y = y1; y < y2; y++){
+                Map[y][x] = 1;
+                visit[y][x] = 1;
+            }
+        }
+        
     }
     
 }
 
+void BFS(){
+    int dx[] = {0, 1, 0, -1};
+    int dy[] = {1, 0, -1, 0};
+    queue<pair<int, int> > q;
+
+    for(int i = 0; i < M; i++){
+        for(int j = 0; j < N; j++){
+            if(visit[i][j] == 0 && Map[i][j] == 0){
+                q.push(make_pair(i,j));
+                visit[i][j] = 1;
+                int count = 1;
+
+                // cout<<"\n==== visit =====\n";
+                // for(int i = 0; i < M; i++){
+                //     for(int j = 0; j < N; j++)
+                //     {
+                //         cout<<visit[i][j];
+                //     }cout<<"\n";
+                // }
+
+                while(!q.empty()){
+                    int x = q.front().first;
+                    int y = q.front().second;
+                    q.pop();
+
+                    for(int idx = 0; idx < 4; idx++){
+                        int nx = x + dx[idx];
+                        int ny = y + dy[idx];
+
+                        if(nx >= 0 && nx < M && ny >= 0 && ny < N){
+                            if(Map[nx][ny] == 0 && visit[nx][ny] == 0){
+                                q.push(make_pair(nx,ny));
+                                visit[nx][ny] = 1;
+                                count++;
+                            }
+                        }
+                            
+                    }
+                }
+                cnt.push_back(count);
+            }
+        }
+    }
+}
+
 int main(){
-    
 
     cin>>M>>N>>K;
 
     init();
 
-    // for(int i = 0; i < K; i++){
-    //     int lx,ly,rx,ry;
-    //     cin>>lx>>ly>>rx>>ry;
-    //     initmap(lx, ly, rx, ry);
+    // cout<<"\n==== Map =====\n";
+    // for(int i = 0; i < M; i++){
+    //     for(int j = 0; j < N; j++)
+    //     {
+    //         cout<<Map[i][j];
+    //     }cout<<"\n";
     // }
 
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < M; j++)
-        {
-            cout<<Map[i][j];
-        }cout<<"\n";
+    // cout<<"\n==== visit =====\n";
+    // for(int i = 0; i < M; i++){
+    //     for(int j = 0; j < N; j++)
+    //     {
+    //         cout<<visit[i][j];
+    //     }cout<<"\n";
+    // }
+
+    BFS();
+
+    sort(cnt.begin(), cnt.end());
+    int size = cnt.size();
+
+    cout<<size<<"\n";
+
+    for(int i = 0; i < size; i++){
+        cout<<cnt[i]<<" ";
     }
             
     return 0;
