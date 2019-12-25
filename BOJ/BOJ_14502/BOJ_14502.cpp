1048a47282a0f19c630b23c8e71605aca7fab0ea
wonbae kim <k1bae2301@gmail.com>
2019-12-25 23:10:46 +0900

    boj

---
+++ BOJ/BOJ_14502/BOJ_14502.cpp
@@ -0,0 +1,26 @@
+// 어렵넴..
+#include<bits/stdc++.h>
+using namespace std;
+
+#define MAX 8
+
+int Map[MAX][MAX] = {0,};
+int visit[MAX][MAX] = {0,};
+int N, M;
+
+int main(){
+    cin>>N>>M;
+
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            cin>>Map[i][j];
+            if(Map[i][j] == 1){
+                visit[i][j] = 1;
+            }
+        }
+    }
+
+
+
+    return 0;
+}
