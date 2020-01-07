eca1dc762c823ac79e946bb69ca569df386e450a
wonbae kim <k1bae2301@gmail.com>
2020-01-07 23:39:19 +0900

    boj nothing

--- BOJ/BOJ_2589/BOJ_2589.cpp
+++ BOJ/BOJ_2589/BOJ_2589.cpp
@@ -1,8 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
+#define MAX 50
 
+int main(){
+    int Map[MAX][MAX] = {0,};
+    string input;
+    int row, col;
+
+    cin>>row>>col;
+
+    for(int i = 0; i < row; i++){
+        for(int j = 0; j < col; j++){
+            
+        }
+    }
 
 
     return 0;
