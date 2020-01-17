37648a1d4743455d7b44d0b1208907aad51aa5aa
wonbae kim <k1bae2301@gmail.com>
2020-01-17 16:46:42 +0900

    boj sort big num

--- BOJ/BOJ_10989/BOJ_10989.cpp
+++ BOJ/BOJ_10989/BOJ_10989.cpp
@@ -5,25 +5,53 @@ using namespace std;
 //int 배열의 최대 크기가 약 이십오만 이란다, 
 // 입력값이 백만 이상이면 그냥 다른방법을 사용하자
 
-int main(){
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-    int num;
-    int array[10001];
-    int T;
+int mycount[10000];
 
-    cin>>T;
+int N;
 
-    for(int i = 0; i < T; i++){
-        cin>>num;
-        array[num]++;
-    }
+int main()
 
-    for(int i = 0; i <10000; i++){
-        for(int j = 0; j < array[i]; j++){
-            cout<<i<<"\n";
-        }
-    }
-    
-    return 0;
+{
+
+
+
+	scanf("%d", &N);
+
+	for (int i = 0; i < N; ++i)
+
+	{
+
+		int num;
+
+		scanf("%d", &num);
+
+
+
+		mycount[num-1]++;
+
+	}
+
+	for (int i = 0; i < 10000; ++i) {
+
+
+
+		if (mycount[i] != 0)
+
+		{
+
+			for (int j = 0; j < mycount[i]; ++j)
+
+			{
+
+				printf("%d \n", i + 1);
+
+			}
+
+		}
+
+	}
+
+	return 0;
+
 }
