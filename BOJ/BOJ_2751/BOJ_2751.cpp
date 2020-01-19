734a2c00fa4472cbb8da5eda1f080fe3df64bf8c
wonbae kim <k1bae2301@gmail.com>
2020-01-20 02:17:33 +0900

    boj easy sort

--- BOJ/BOJ_2751/BOJ_2751.cpp
+++ BOJ/BOJ_2751/BOJ_2751.cpp
@@ -4,6 +4,9 @@ using namespace std;
 #define MAX 1000000
 
 int main(){
+    //672ms -> 280ms
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    
     int n;
     int arr[MAX];
 
