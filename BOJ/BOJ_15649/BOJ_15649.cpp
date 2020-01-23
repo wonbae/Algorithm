9e6e83bdebf5cc1bccf9e3ad82ae0b36842f4455
wonbae kim <k1bae2301@gmail.com>
2020-01-23 20:43:28 +0900

    boj N,M s3 not solved

--- BOJ/BOJ_15649/BOJ_15649.cpp
+++ BOJ/BOJ_15649/BOJ_15649.cpp
@@ -1,13 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int permutation(int p, int q){
+    if(q == 0 || p == q) return 1;
+
+    return permutation(p - 1, q) + permutation(p - 1, q - 1);
+}
+
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(NULL);
 
+    vector<int> v;
     int n,m;
     cin>>n>>m;
 
+    // for(int i = 1; i <= n; i++){
+    //     v.push_back(i);
+    // }
+
+    // do{
+    //     for(int i = 0; i < m; i++){
+    //         cout<<v[i]<<" ";
+    //     }cout<<"\n";
+
+    // }while(next_permutation(v.begin(), v.begin() + m));
     
+    permutation(n,m);
 
 
     return 0;
