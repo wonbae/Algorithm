a5930d2d2dbd483090bbc4ac24e31c9d20dcd9da
wonbae kim <k1bae2301@gmail.com>
2020-02-18 21:41:31 +0900

    boj s3 N과M 순열

--- BOJ/BOJ_15649/BOJ_15649.cpp
+++ BOJ/BOJ_15649/BOJ_15649.cpp
@@ -1,32 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int permutation(int p, int q){
-    if(q == 0 || p == q) return 1;
+int n,m;
+bool visit[9];  // 전역변순 true로 초기화 됨
+vector<int> v;
 
-    return permutation(p - 1, q) + permutation(p - 1, q - 1);
+void DFS(int cnt){
+    if(cnt == m){
+        for(int i = 0; i < v.size(); i++){
+            cout<<v.at(i)<<" ";
+        }
+        cout<<"\n";
+    }
+
+    for(int i = 0; i < n; i++){
+        if(!visit[i]){
+            v.push_back(i + 1);
+            visit[i] = true;
+            DFS(cnt + 1);
+            v.pop_back();
+            visit[i] = false;
+        }
+    }
+
 }
 
 int main(){
-    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL); cout.tie(NULL);
 
-    vector<int> v;
-    int n,m;
     cin>>n>>m;
 
-    // for(int i = 1; i <= n; i++){
-    //     v.push_back(i);
-    // }
-
-    // do{
-    //     for(int i = 0; i < m; i++){
-    //         cout<<v[i]<<" ";
-    //     }cout<<"\n";
-
-    // }while(next_permutation(v.begin(), v.begin() + m));
-    
-    permutation(n,m);
-
+    DFS(0);
 
     return 0;
 }
