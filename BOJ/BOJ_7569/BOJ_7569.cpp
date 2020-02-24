ef16e3419ada95c37279776074ac1911e2595c52
wonbae kim <k1bae2301@gmail.com>
2020-02-24 23:50:02 +0900

    boj s1 tomato 3d

--- BOJ/BOJ_7569/BOJ_7569.cpp
+++ BOJ/BOJ_7569/BOJ_7569.cpp
@@ -1,11 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 #define MAX 100
-
-int tomato[MAX][MAX][MAX] = {0,};
-bool visit[MAX][MAX][MAX] = {false,};
-int M,N,H;
 
 typedef struct TOMA{
     int y,x,z;
@@ -16,6 +11,12 @@ typedef struct TOMA{
     }
 
 }TOMA;
+
+int tomato[MAX][MAX][MAX];
+// bool visit[MAX][MAX][MAX];
+int M,N,H;
+queue<TOMA> Q;
+int cnt = -1;
 
 void PRINT(){
     cout<<"\n====== Print Tomato =======\n";
@@ -28,9 +29,29 @@ void PRINT(){
     }
 }
 
+bool iszero(){
+    for(int z = 0; z < H; z++){
+        for(int y = 0; y < N; y++){
+            for(int x = 0; x < M; x++){
+                if(tomato[y][x][z] == 0){
+                    return 1;
+                }
+
+                if(tomato[y][x][z] > cnt){
+                     cnt = tomato[y][x][z];
+                } 
+            }
+        }
+    }
+    return 0;
+}
+
 int main(){    
-    queue<pair<int, int> > q;
-    queue<TOMA> Q;
+    // queue<pair<int, int> > q;
+    int dy[] = {-1, 0, 1, 0, 0, 0};
+    int dx[] = {0, 1, 0, -1, 0, 0};
+    int dz[] = {0, 0, 0, 0, -1, 1};
+    
     
     cin>>M>>N>>H;
 
@@ -38,32 +59,46 @@ int main(){
         for(int y = 0; y < N; y++){
             for(int x = 0; x < M; x++){
                 cin>>tomato[y][x][z];
-            }
-        }
-    }
-
-    PRINT();
-
-    for(int z = 0; z < H; z++){
-        for(int y = 0; y < N; y++){
-            for(int x = 0; x < M; x++){
+                // if(tomato[y][x][z] == -1) visit[y][x][z] = true; 
                 if(tomato[y][x][z] == 1){
                     Q.push(TOMA(y,x,z));
-                
-                    while(!Q.empty()){
-                        int y = Q.front().y;
-                        int x = Q.front().x;
-                        int z = Q.front().z;
-                        Q.pop();
-
-                        cout<<"y : "<<y<<", x : "<<x<<", z : "<<z<<"\n";
-
-                    }
+                    // visit[y][x][z] = true;
                 }
             }
         }
     }
 
+    while(!Q.empty()){
+        int y = Q.front().y, x = Q.front().x, z = Q.front().z;
+        Q.pop();
+
+        for(int idx = 0; idx < 6; idx++){
+            int ny = y + dy[idx];
+            int nx = x + dx[idx];
+            int nz = z + dz[idx];
+                
+            if(ny < 0 || nx < 0 || nz < 0 || ny >= N || nx >= M || nz >= H) continue;
+            // if(visit[ny][nx][nz]) continue;
+            if(tomato[ny][nx][nz]) continue;
+            if(tomato[ny][nx][nz] == -1) continue;
+
+            // visit[ny][nx][nz] = true;
+            tomato[ny][nx][nz] = tomato[y][x][z] + 1;
+            Q.push(TOMA(ny, nx, nz));
+            
+            
+        }
+        // PRINT();
+    }
+
+    
+    if(iszero()){
+        cout<<-1<<"\n";
+        return 0;
+    }else{
+        cout<<cnt - 1<<"\n";
+        return 0;
+    }
 
     return 0;
 }
