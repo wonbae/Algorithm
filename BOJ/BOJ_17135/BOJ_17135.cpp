e1674064cb2e755c3d6c30ed3176527b2c1ad468
wonbae kim <k1bae2301@gmail.com>
2020-03-17 20:34:29 +0900

    boj 맞왜틀?

--- BOJ/BOJ_17135/BOJ_17135.cpp
+++ BOJ/BOJ_17135/BOJ_17135.cpp
@@ -1,86 +1,72 @@
 // boj_17135 Castle Defense G4
+// row행, col열을 사용할 때, y를 row가 아닌 col로 생각하고 풀음.. 고로 row = x, col = y 나의 나쁜습관임
 #include<bits/stdc++.h>
 using namespace std;
 #define SIZE 15
 
+typedef pair<int, pair<int, int> > piii;
 int N, M, D;
 int Map[SIZE][SIZE];
 int tmp[SIZE][SIZE];
-bool visit[SIZE][SIZE];
-const int dx[] = {0, -1, 0, 1};
-const int dy[] = {-1, 0, 1, 0};
-int answer, ecnt;
+int answer;
 
-int dist(int nx, int ny, int x, int y){
+int distance(int nx, int ny, int x, int y){
     return abs(nx - x) + abs(ny - y);
 }
 
 void kill(){
-    cout<<"\n++++++++ kill start ++++++++\n";
-    memcpy(tmp, Map, sizeof(Map));
-    ecnt = 0;
-    int term = N;
+    // cout<<"\n +++++ new Archer ++++++\n";
     vector<pair<int, int> > archer;
-    vector<pair<int, int> > enermy;
+    memcpy(tmp, Map, sizeof(Map));
+    int term = N;
+    int ecnt = 0;
 
-    cout<<"\n";
-    cout<<"answer : "<<answer<<"\n";
-    for(int r = 0; r <= N; r++){
-        for(int c = 0; c < M; c++){
-            cout<<tmp[r][c]<<" ";
-        }cout<<"\n";
+    for(int i = 0; i < M; i++){
+        if(Map[N][i] == 2) archer.push_back(make_pair(N, i));
     }
-    
-    while(term){
-        for(int c = 0; c < M; c++){
-            if(Map[term][c] == 2)
-                archer.push_back(make_pair(term, c));
-        }    
- 
+
+    while(term--){
+        vector<pair<int, int> > enermy;
         for(int i = 0; i < 3; i++){
-            int shortest = 987654321, a = 0, b = 0;
-            for(int r = term - 1; r >= 0; r--){
+            priority_queue<piii, vector<piii>, greater<piii> > q2;    //1. distance 2.column 3.row / Ascending 오름차순
+            for(int r = 0; r < N; r++){
                 for(int c = 0; c < M; c++){
                     if(tmp[r][c] == 0) continue;
                     if(tmp[r][c] == 1){
-                        int x = archer[i].first;
-                        int y = archer[i].second;
-
-                        if(dist(x, y, r, c) <= D){
-                            shortest = min(shortest, dist(x, y, r, c)); 
-                            enermy.push_back(make_pair(a, b));
-                        }else continue;
+                        int dist = distance(archer[i].first, archer[i].second, r, c);
+                        if(dist <= D){
+                            q2.push(make_pair(dist, make_pair(c, r)));  // pair안에선 앞에꺼 먼저 비교함 그래서 c 먼저 넣음
+                        } 
                     }
                 }
             }
-            
+            if(q2.size()){
+                int y = q2.top().second.first, x = q2.top().second.second;
+                // cout<<"d : "<<q2.top().first<<", r : "<<x<<", c : "<<y<<"\n";
+                enermy.push_back(make_pair(x, y));
+            }
         }
 
-        for(int i = 0; i < enermy.size(); i++){
-            cout<<"("<<enermy[i].first<<", "<<enermy[i].second<<")\n";
-            ecnt++;
-            tmp[enermy[i].first][enermy[i].second] = 0;
+        //erase enermy
+        for(int i = 0; i < (int)enermy.size(); i++){
+            int x = enermy[i].first, y = enermy[i].second;
+            if(tmp[x][y]){
+                tmp[x][y] = 0;
+                ecnt++;
+            }
         }
 
-        // cout<<"\n";
-        // cout<<"answer : "<<answer<<"\n";
-        // for(int r = 0; r <= N; r++){
-        //     for(int c = 0; c < M; c++){
-        //         cout<<tmp[r][c]<<" ";
-        //     }cout<<"\n";
-        // }
-        
-        for(int c = 0; c < M; c++){
-            if(term - 1 < 0) break;
-            tmp[term - 1][c] = 0;
-            tmp[term - 1][c] = tmp[term][c];
-            tmp[term][c] = 0;
+        //move down
+        for(int r = N - 2; r >= 0; r--){
+            for(int c = 0; c < M; c++){
+                tmp[r + 1][c] = tmp[r][c];
+            }
         }
-
-        if(answer < ecnt) answer = ecnt;
-        term--;
-        archer.clear();
+        for(int c = 0; c < M; c++) tmp[0][c] = 0;
     }
+
+    if(answer < ecnt) answer = ecnt;
+
 }
 
 void nCrArcher(int col, int cnt){
