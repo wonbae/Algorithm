8fd8bc073b864813448c2762a5b11dbe80a25187
wonbae kim <k1bae2301@gmail.com>
2020-03-17 18:02:26 +0900

    boj sival jot got da

--- BOJ/BOJ_17135/BOJ_17135.cpp
+++ BOJ/BOJ_17135/BOJ_17135.cpp
@@ -9,75 +9,97 @@ int tmp[SIZE][SIZE];
 bool visit[SIZE][SIZE];
 const int dx[] = {0, -1, 0, 1};
 const int dy[] = {-1, 0, 1, 0};
-int enermy;
+int answer, ecnt;
 
 int dist(int nx, int ny, int x, int y){
     return abs(nx - x) + abs(ny - y);
 }
 
 void kill(){
-    queue<pair<int, int> > q;
+    cout<<"\n++++++++ kill start ++++++++\n";
+    memcpy(tmp, Map, sizeof(Map));
+    ecnt = 0;
     int term = N;
-    enermy = 0;
+    vector<pair<int, int> > archer;
+    vector<pair<int, int> > enermy;
 
-    while(term--){
+    cout<<"\n";
+    cout<<"answer : "<<answer<<"\n";
+    for(int r = 0; r <= N; r++){
         for(int c = 0; c < M; c++){
-            if(Map[term + 1][c] == 2){
-                memset(visit, false, sizeof(visit));
-                memset(tmp, 0, sizeof(tmp));
+            cout<<tmp[r][c]<<" ";
+        }cout<<"\n";
+    }
+    
+    while(term){
+        for(int c = 0; c < M; c++){
+            if(Map[term][c] == 2)
+                archer.push_back(make_pair(term, c));
+        }    
+ 
+        for(int i = 0; i < 3; i++){
+            int shortest = 987654321, a = 0, b = 0;
+            for(int r = term - 1; r >= 0; r--){
+                for(int c = 0; c < M; c++){
+                    if(tmp[r][c] == 0) continue;
+                    if(tmp[r][c] == 1){
+                        int x = archer[i].first;
+                        int y = archer[i].second;
 
-                q.push(make_pair(term + 1, c));
-                visit[term + 1][c] = true;
-
-                while(!q.empty()){
-                    int x = q.front().first;
-                    int y = q.front().second;
-                    q.pop();
-
-                    for(int idx = 0; idx < 4; idx++){
-                        int nx = x + dx[idx];
-                        int ny = y + dy[idx];
-
-                        if(nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
-                        if(visit[nx][ny]) continue;
-                        if(tmp[nx][ny] > D) continue;
-
-                        if(Map[nx][ny] == 1 && dist(nx, ny, term + 1, c) <= D){
-                            enermy++;
-                            
-                        }
-                        
-                            tmp[nx][ny] += 1;
-                            visit[nx][ny] = true;
-                            q.push(make_pair(nx, ny));
-                        
-                        
+                        if(dist(x, y, r, c) <= D){
+                            shortest = min(shortest, dist(x, y, r, c)); 
+                            enermy.push_back(make_pair(a, b));
+                        }else continue;
                     }
                 }
             }
+            
         }
+
+        for(int i = 0; i < enermy.size(); i++){
+            cout<<"("<<enermy[i].first<<", "<<enermy[i].second<<")\n";
+            ecnt++;
+            tmp[enermy[i].first][enermy[i].second] = 0;
+        }
+
+        // cout<<"\n";
+        // cout<<"answer : "<<answer<<"\n";
+        // for(int r = 0; r <= N; r++){
+        //     for(int c = 0; c < M; c++){
+        //         cout<<tmp[r][c]<<" ";
+        //     }cout<<"\n";
+        // }
+        
+        for(int c = 0; c < M; c++){
+            if(term - 1 < 0) break;
+            tmp[term - 1][c] = 0;
+            tmp[term - 1][c] = tmp[term][c];
+            tmp[term][c] = 0;
+        }
+
+        if(answer < ecnt) answer = ecnt;
+        term--;
+        archer.clear();
     }
 }
 
 void nCrArcher(int col, int cnt){
     if(cnt == 3){
         // for(int i = 0; i < M; i++){
-        //     cout<<Map[N + 1][i]<<" ";
+        //     cout<<Map[N][i]<<" ";
         // }
         // cout<<"\n";
         kill();
         return;
     }
 
-    for(int i = 0; i < M; i++){
-        if(Map[N + 1][i] == 2) continue;
+    for(int i = col; i < M; i++){
+        if(Map[N][i] == 2) continue;
 
-        Map[N + 1][i] = 2;
+        Map[N][i] = 2;
         nCrArcher(col + 1, cnt + 1);
-        Map[N + 1][i] = 0;
+        Map[N][i] = 0;
     }
-    
-
 }
 
 int main(){
@@ -93,6 +115,8 @@ int main(){
     }
 
     nCrArcher(0, 0);
+
+    cout<<answer<<"\n";
 
     return 0;
 }
