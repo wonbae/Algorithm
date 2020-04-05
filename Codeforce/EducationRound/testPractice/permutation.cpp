5ba0636f22efa027317fc2eca906d2a919449fd2
wonbae kim <k1bae2301@gmail.com>
2020-04-05 19:04:38 +0900

    nothing

---
+++ Codeforce/EducationRound/testPractice/permutation.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> v;
+int idx, cnt = 0;
+
+void nCr(int idx, int cnt){
+    if(cnt == 3){
+        
+    }
+
+}
+
+int main(){
+    
+    int a,b,c;
+
+    cin>>a>>b>>c>>idx;
+
+    v.push_back(a);
+    v.push_back(b);
+    v.push_back(c);
+
+    nCr(0, 0);
+
+
+
+
+    return 0;
+}
