e49c14aa70df8f8399bed3e345db810a6590ed78
wonbae kim <k1bae2301@gmail.com>
2020-04-21 18:34:53 +0900

    boj s5 파스칼삼각형

--- BOJ/BOJ_16395/BOJ_16395.cpp
+++ BOJ/BOJ_16395/BOJ_16395.cpp
@@ -2,14 +2,11 @@
 using namespace std;
 
 int nCr(int n, int r){
-
     if(n == 0 || r == 0 || n == r){
         return 1;
     }
 
     return nCr(n - 1, r - 1) + nCr(n - 1, r);
-
-    
 }
 int main(){
     ios_base::sync_with_stdio(0);
