373fd8ca83e486974c0be356bb0c9ce825224868
wonbae kim <k1bae2301@gmail.com>
2020-05-09 22:53:50 +0900

    tow pointer

--- Codeforce/justCode/40/40.cpp
+++ Codeforce/justCode/40/40.cpp
@@ -5,9 +5,9 @@ using namespace std;
 
 int main(){
     freopen("input.txt", "rt", stdin);
-    int n, n2, p1 = 0, p2 = 0, p3 = 0;
+    int n, n2, n3, p1 = 0, p2 = 0, p3 = 0;
     cin>>n;
-    vector<int> a(n), c;
+    vector<int> a(n);
     for(int i = 0; i < n; i++){
         cin>>a[i];
     }
@@ -17,14 +17,18 @@ int main(){
     for(int i = 0; i < n2; i++){
         cin>>b[i];
     }
+    
+    n3 = n2 > n ? n2 : n;
+    vector<int> c(n3);
 
     sort(a.begin(), a.end());
     sort(b.begin(), b.end());
 
     while(p1 < n && p2 < n2){
         if(a[p1] == b[p2]){
-            // c[p3++] = a[p1];
-            c.push_back(a[p1]);
+            // c.push_back(a[p1]);
+            c[p3++] = a[p1++];
+            p2++;
         }else if(a[p1] < b[p2]){
             p1++;
         }else{
@@ -32,7 +36,7 @@ int main(){
         }
     }
 
-    for(int i = 0; i < c.size(); i++){
+    for(int i = 0; i < p3; i++){
         cout<<c[i]<<" ";
     }
 
