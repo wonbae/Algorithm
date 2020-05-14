f61c129f9fc76125aed69f8fc8c8049ba4f1e4c6
wonbae kim <k1bae2301@gmail.com>
2020-05-14 18:34:15 +0900

    boj G2 가장 긴 증가하는 부분수열2 (LIS, lower_bound)

---
+++ BOJ/BOJ_12015/BOJ_12015.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int N, num;
+    vector<int> v;
+    cin>>N;
+    cin>>num;
+    v.push_back(num);
+
+    for(int i = 1; i < N; i++){
+        cin>>num;
+        if(num > v.back()){
+            v.push_back(num);
+        }else{
+            int idx = lower_bound(v.begin(), v.end(), num) - v.begin();
+            v[idx] = num;
+        }
+    }
+
+    cout<<v.size()<<"\n";
+    return 0;
+}
