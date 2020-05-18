fec21767d494c384d059b5d25dcc67879b8f0722
wonbae kim <k1bae2301@gmail.com>
2020-05-19 03:20:22 +0900

    not solved, why fail?

---
+++ BOJ/BOJ_14002/BOJ_14002.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+typedef long long ll;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+    
+    int N;
+    ll num;
+    cin>>N;
+    vector<ll> v;
+    cin>>num;
+    v.push_back(num);
+
+    for(int i = 1; i < N; i++){
+        cin>>num;
+        if(v.back() < num){
+            v.push_back(num);
+        }else{
+            ll idx = lower_bound(v.begin(), v.end(), num) - v.begin();
+            v[idx] = num;
+        }
+    }
+
+    cout<<v.size()<<"\n";
+    for(int i = 0; i < v.size(); i++){
+        cout<<v[i]<<" ";
+    }
+    return 0;
+}
