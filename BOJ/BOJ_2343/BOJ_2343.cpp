142da899ee856b957c21a707030ddb0f48673036
wonbae kim <k1bae2301@gmail.com>
2020-05-19 04:21:50 +0900

    boj s1 기타레슨 (이분탐색)

---
+++ BOJ/BOJ_2343/BOJ_2343.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int n, v[100001];
+
+int cal(int s){
+    int cnt = 1, sum = 0;
+    for(int i = 0; i < n; i++){
+        if(sum + v[i] > s){
+            cnt++;
+            sum = v[i];
+        }else{
+            sum += v[i];
+        }
+    }
+    return cnt;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int m, lt = 0, rt = 0, mid = 0, res = 0, maxi = -2147000000;
+    cin>>n>>m;
+    
+    for(int i = 1; i <= n; i++){
+        cin>>v[i - 1];
+        rt += v[i - 1];
+        if(v[i - 1] > maxi) maxi = v[i - 1];
+    }
+
+    while(lt <= rt){
+        mid = (lt + rt) / 2;
+        
+        if(mid >= maxi && cal(mid) <= m){
+            res = mid;
+            rt = mid - 1;
+        }else{
+            lt = mid + 1;
+        }
+    }
+
+    cout<<res<<"\n";
+    return 0;
+}
