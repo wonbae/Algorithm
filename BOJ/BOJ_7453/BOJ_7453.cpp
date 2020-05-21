3dbcbb99b4840fb4fcc4b836ffd91eb27a16c8f1
wonbae kim <k1bae2301@gmail.com>
2020-05-21 20:37:45 +0900

    boj G2 합이 0인 네 정수(Binarysearch)

--- BOJ/BOJ_7453/BOJ_7453.cpp
+++ BOJ/BOJ_7453/BOJ_7453.cpp
@@ -1,27 +1,34 @@
 #include<iostream>
-#include<vector>
 #include<algorithm>
 using namespace std;
 typedef long long ll;
+
+int a[4001], b[4001], c[4001], d[4001];
+int combine1[16000001], combine2[16000001];
+
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     int N;
     cin>>N;
-    vector<int> a(N), b(N), c(N), d(N);
     for(int i = 0; i < N; i++){
         cin>>a[i]>>b[i]>>c[i]>>d[i];
     }
 
-    sort(a.begin(), a.end());
-    sort(b.begin(), b.end());
-    sort(c.begin(), c.end());
-    sort(d.begin(), d.end());
-
     for(int i = 0; i < N; i++){
-        cout<<a[i]<<" "<<b[i]<<" "<<c[i]<<" "<<d[i]<<"\n";
+        for(int j = 0; j < N; j++){
+            combine1[i*N + j] = a[i] + b[j];
+            combine2[i*N + j] = c[i] + d[j];
+        }
     }
 
+    sort(combine2, combine2+(N*N));
 
+    ll res = 0;
+    for(int i = 0; i < N*N; i++){
+        res += (upper_bound(combine2, combine2+(N*N), -combine1[i]) - lower_bound(combine2, combine2+(N*N), -combine1[i]));
+    }
+
+    cout<<res<<"\n";
     return 0;
 }
