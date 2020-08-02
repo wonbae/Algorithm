f040330dc6f441451767b3edbe191dcbc7556636
wonbae kim <k1bae2301@gmail.com>
2020-08-02 21:58:38 +0900

    need more change(Three pointer)

--- Codeforce/justCode/52/52.cpp
+++ Codeforce/justCode/52/52.cpp
@@ -1,24 +1,30 @@
+// Three pointer
 #include<iostream>
 #include<vector>
 using namespace std;
 
+int MINI(int a, int b, int c){
+    int tmp = a > b ? b : a;
+    return tmp > c ? c : tmp;
+}
 int main(){
-    int N, i = 1;
+    int N;
     cin>>N;
-    vector<int> v;
-    v.push_back(i);
-    i++;
-    while(1){
-        if(i % 2 == 0 || i % 5 == 0 || i % 3 == 0){
-            v.push_back(i);
-            cout<<i<<"\n";
-            if(v.size() == N){
-                cout<<v[N - 1]<<"\n";
-                break;
-            }
-        }
-        i++;
+    int arr[1501] = {0,};
+    
+    int p2 = 1, p3 = 1, p5 = 1;
+
+    for(int i = 1; i <= N; i++){
+        int min = 2147000000;
+        min = MINI(p2 * 2, p3 * 3, p5 * 5);
+        arr[i] = min;
+        
+        if(min == p2 * 2) p2++;
+        else if(min == p3 * 3) p3++;
+        else p5++;
+
     }
+
 
 
     return 0;
