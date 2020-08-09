703c01b941ee6f28be2c5ccec7d39fcc1da8b888
wonbae kim <k1bae2301@gmail.com>
2020-08-10 01:29:42 +0900

    DFS pre,iner,post - order

--- Codeforce/justCode/58/58.cpp
+++ Codeforce/justCode/58/58.cpp
@@ -3,14 +3,19 @@
 using namespace std;
 
 void DFS(int a){
+    if(a > 7) return ;
+    
+    // cout<<a; 전위수회
+    DFS(a*2);
+    // cout<<a; 중위순회
+    DFS(a*2+1);
+    cout<<a;    //후위순회
     
 }
 
 int main(){
-    int n;
-    cin>>n;
 
-    DFS(n);
+    DFS(1);
 
     return 0;
 }
