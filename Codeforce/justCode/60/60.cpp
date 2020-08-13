c6db757664ef101d4dccc7590eeeafef6d669da1
wonbae kim <k1bae2301@gmail.com>
2020-08-13 21:23:20 +0900

    합이같은 부분집합 DFS

--- Codeforce/justCode/60/60.cpp
+++ Codeforce/justCode/60/60.cpp
@@ -4,8 +4,10 @@ using namespace std;
 int nums[10] = {0,};
 bool ch[10] = {false,};
 int n, a = 0, b = 0;
+bool flag = false;
 
 void DFS(int depth){
+    if(flag) return ;
     if(depth > n){
         for(int i = 0; i < n; i++){
             if(ch[i]){
@@ -16,9 +18,10 @@ void DFS(int depth){
         }
         if(a == b){
             cout<<"YES"<<"\n";
+            flag = true;
             return ;
-        }
-        return ;
+        }else flag = false;
+        
     }else{
         ch[depth] = true;
         DFS(depth + 1);
@@ -35,6 +38,8 @@ int main(){
     }
 
     DFS(1);
+
+    if(!flag) cout<<"NO";
 
     return 0;
 }
