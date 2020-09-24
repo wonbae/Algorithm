a63a5d39725d874ec432246e7d1b580f4cce5e43
wonbae kim <k1bae2301@gmail.com>
2020-09-24 20:33:19 +0900

    문장열

---
+++ BOJ/BOJ_1120/BOJ_1120.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int cnt = 0, mini = 2147000000;
+    string a, b;
+    cin>>a>>b;
+
+    for(int i = 0; i <= b.length() - a.length(); i++){
+        cnt = 0;
+        for(int j = 0; j < a.length(); j++){
+            if(a[j] != b[j + 1]) cnt++;
+        }
+        if(mini > cnt) mini = cnt;
+    }
+    cout<<mini<<"\n";
+    return 0;
+}
