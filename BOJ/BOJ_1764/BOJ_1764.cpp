46a04d354da9dc0261e5454b29623f4d1b419e79
wonbae kim <k1bae2301@gmail.com>
2020-09-24 19:11:54 +0900

    문자열 이분탐색

--- BOJ/BOJ_1764/BOJ_1764.cpp
+++ BOJ/BOJ_1764/BOJ_1764.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <algorithm>
-
 using namespace std;
+
+string a[500001], b[500001], c[500001];
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(NULL);
-
-    string a[500001], b[500001], c[500001];
+    
     int N, M, idx = 0, aidx = 0, bidx = 0;
     cin>>N>>M;
     
@@ -21,8 +21,21 @@ int main(){
     sort(a, a + N);
     sort(b, b + M);
 
-    
+    while(aidx < N && bidx < M){
+        if(a[aidx] == b[bidx]){
+            c[idx++] = a[aidx++];
+            bidx++;
+        }else if(a[aidx] > b[bidx]){
+            bidx++;
+        }else{
+            aidx++;
+        }
+    }
 
+    cout<<idx<<"\n";
+    for(int i = 0; i < idx; i++){
+        cout<<c[i]<<"\n";
+    }
 
     return 0;
 }
