8a95325f8e2fbc76b4ed4e4d3a7bccd31a9fbaab
wonbae kim <k1bae2301@gmail.com>
2020-10-06 17:36:11 +0900

    Disjoint-set(Union-Find)

--- Codeforce/justCode/77/77.cpp
+++ Codeforce/justCode/77/77.cpp
@@ -1,16 +1,39 @@
-#include <string>
-#include <vector>
 #include <iostream>
 using namespace std;
 
+int unf[1001];
+
+int find(int v){
+    if(v == unf[v]) return v;
+    return unf[v] = find(unf[v]);
+}
+
+void UNION(int a, int b){
+    a = find(a);
+    b = find(b);
+    if(a != b) unf[a] = b;
+}
+
 int main(){
-    int a[1000001];
-    for(int i = 0; i < 1000000; i++){
-        a[i] = i + 1;
+    ios_base::sync_with_stdio(false);
+    freopen("input.txt", "rt", stdin);
+    
+    int n, m, a, b;
+    
+    cin>>n>>m;
+    for(int i = 1; i <= n; i++){
+        unf[i] = i;
     }
 
-    for(int i = 0; i < 1000000; i++){
-        cout<<a[i]<<" ";
+    for(int i = 0; i < m; i++){
+        cin>>a>>b;
+        UNION(a, b);
     }
+
+    cin>>a>>b;
+    a = find(a);
+    b = find(b);
+    if(a == b) cout<<"YES";
+    else cout<<"NO";
     return 0;
 }
