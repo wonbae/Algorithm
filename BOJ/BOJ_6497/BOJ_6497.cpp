e9606cfa0428c8463532667e98d5d427b577bc52
wonbae kim <k1bae2301@gmail.com>
2020-10-12 22:51:45 +0900

    MST(Kruskal, Prim) 전력난 G4

--- BOJ/BOJ_6497/BOJ_6497.cpp
+++ BOJ/BOJ_6497/BOJ_6497.cpp
@@ -1,3 +1,4 @@
+// Kruskal - 212ms
 #include<iostream>
 #include<vector>
 #include<queue>
@@ -68,3 +69,62 @@ int main(){
 
     return 0;
 }
+
+// //Prim - 404ms
+// #include<iostream>
+// #include<vector>
+// #include<queue>
+// using namespace std;
+
+// struct edge{
+//     int ee;
+//     int val;
+//     edge(int a, int b){
+//         ee = a;
+//         val = b;
+//     }
+//     bool operator<(const edge& w) const{
+//         return val > w.val;
+//     }
+// };
+
+// int main(){
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+//     int n, m, x, y, z;
+
+//     while(1){
+//         cin>>n>>m;
+//         if(n == 0 && m == 0) return 0;
+
+//         vector<pair<int, int> > v[200001];
+//         priority_queue<edge> pq;
+//         int ch[200001] = {0,};
+//         int sum = 0;
+
+//         for(int i = 0; i < m; i++){
+//             cin>>x>>y>>z;
+//             sum += z;
+//             v[x].push_back(make_pair(y, z));
+//             v[y].push_back(make_pair(x, z));
+//         }
+
+//         int cost = 0;
+//         pq.push(edge(0, 0));
+//         while(!pq.empty()){
+//             int now = pq.top().ee;
+//             int value = pq.top().val;
+//             pq.pop();
+
+//             if(ch[now] == 0){
+//                 cost += value;
+//                 ch[now] = 1;
+//                 for(int i = 0; i < v[now].size(); i++){
+//                     pq.push(edge(v[now][i].first, v[now][i].second));
+//                 }
+//             }
+//         }
+//         cout<<sum - cost<<"\n";
+//     }
+//     return 0;
+// }
