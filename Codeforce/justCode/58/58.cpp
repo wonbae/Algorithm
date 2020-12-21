48578130d67fe771269f41929e1994240de1c67b
wonbae kim <k1bae2301@gmail.com>
2020-12-22 03:25:38 +0900

    tree

--- Codeforce/justCode/58/58.cpp
+++ Codeforce/justCode/58/58.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 
 void DFS(int a){
-    if(a > 7) return ;
+    if(a > 10) return ;
     
     // cout<<a; 전위수회
     DFS(a*2);
-    // cout<<a; 중위순회
+    cout<<a<<" "; //중위순회
     DFS(a*2+1);
-    cout<<a;    //후위순회
+    // cout<<a;    //후위순회
     
 }
 
