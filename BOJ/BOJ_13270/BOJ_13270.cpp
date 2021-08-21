9e3292b1fd73a798ee5dbe15c4576927ee8058d5
soooupp <k1bae2301@gmail.com>
2021-08-21 17:07:00 +0900

    피보나치 치킨, 수학, 디피

---
+++ BOJ/BOJ_13270/BOJ_13270.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0),cin.tie(NULL);
+    int N;
+    cin>>N;
+
+    cout<<(N+1)/2<<" "<<(N*2)/3;
+
+    return 0;
+}
