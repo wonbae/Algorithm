d844ea6dfe03af20110001c59eb8cc8c2df6c3eb
soooupp <k1bae2301@gmail.com>
2021-08-23 22:13:33 +0900

    학교 탐방하기, MST, Kruskal

--- BOJ/BOJ_13418/BOJ_13418.cpp
+++ BOJ/BOJ_13418/BOJ_13418.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 int unf[1001];
+int N,M;
 
 struct edge{
     int s,e,w;
@@ -28,8 +29,19 @@ void Union(int a, int b){
     if(a!=b) unf[a] = b;
 }
 
+bool cmp(edge a, edge b){
+    return a.w > b.w;
+}
+
+void init(){
+    for(int i = 1; i <= N; i++){
+        unf[i] = i;
+    }
+    unf[0] = 0;
+}
+
 int main(){
-    int N,M;
+    ios_base::sync_with_stdio(false), cin.tie(NULL);
     int a, b, c;
     cin>>N>>M;
     vector<edge> v;
@@ -44,14 +56,33 @@ int main(){
 
     sort(v.begin(), v.end());
 
-    for(int i = 0; i < N; i++){
-        unf[i] = i;
-    }
+    init();
+
+    int ohru = 0, naeri = 0;
 
     for(int i = 0; i < v.size(); i++){
-        
+        if(Find(v[i].s) != Find(v[i].e)){
+            Union(v[i].s, v[i].e);
+            if(v[i].w == 0){
+                ohru++;
+            }
+        }
     }
 
+    sort(v.begin(), v.end(), cmp);
+
+    init();
+
+    for(int i = 0; i < v.size(); i++){
+        if(Find(v[i].s) != Find(v[i].e)){
+            Union(v[i].s, v[i].e);
+            if(v[i].w == 0){
+                naeri++;
+            }
+        }
+    }
+
+    cout<<(ohru*ohru)-(naeri*naeri)<<"\n";
 
     return 0;
 }
