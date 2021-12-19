0d4029daa4dffa1b9aa580fa4e6b5962cf4d9de0
wonbae <k1bae2301@gmail.com>
2021-12-19 13:03:14 +0900

    LCS

--- BOJ/BOJ_9251/BOJ_9251.cpp
+++ BOJ/BOJ_9251/BOJ_9251.cpp
@@ -1,20 +1,27 @@
-// // LCS(Longest Common Subsequence, 최장 공통 부분 수열)미완성
+// // LCS(Longest Common Subsequence, 최장 공통 부분 수열)
 
 #include <iostream>
 #include <string>
-#include <algorithm>
 using namespace std;
 
 int main(){
-    string str[1000];
-    cin >> str[0];
-    int len = str[0].size();
-    
-    for(int i=1; i<len; i++){
-        str[i] = str[0].substr(i);
-        cout<< str[i] <<endl;
+    ios_base::sync_with_stdio(false), cin.tie(NULL);
+    string str1, str2;
+    cin>>str1>>str2;
+
+    int len1 = str1.length();
+    int len2 = str2.length();
+
+    int dp[1001][1001] = {0,};
+
+    for(int i = 0; i < len1; i++){
+        for(int j = 0; j < len2; j++){
+            if(str1[i] == str2[j]){
+                dp[i+1][j+1] = dp[i][j] + 1;
+            }else{
+                dp[i+1][j+1] = max(dp[i+1][j], dp[i][j+1]);
+            }
+        }
     }
-    sort(str, str+len);
-    for(int i=0; i<len; i++)
-        cout << str[i] << endl;
+    cout<<dp[len1][len2];
 }
