c3705bfcb8d63df5a64b248f02b2c9e78772e0c6
wonbae kim <k1bae2301@gmail.com>
2022-06-22 16:00:13 +0900

    1

---
+++ BOJ/BOJ_4375/BOJ_4375.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    while(scanf("%d", &n) != EOF){
+        int num=1, cnt=1;
+        while(true){
+            if(num%n==0){
+                printf("%d",cnt);
+                break;
+            }else{
+                num = (num*10)+1;
+                num %= n;
+                cnt+=1;
+            }
+        }
+    }
+
+    return 0;
+}
