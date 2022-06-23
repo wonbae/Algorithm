47f3ec1585507cfe6fe48179516e42008db1a69b
wonbae kim <k1bae2301@gmail.com>
2022-06-23 23:10:10 +0900

    나는야 포켓몬 마스터 이다솜

--- BOJ/BOJ_1620/BOJ_1620.cpp
+++ BOJ/BOJ_1620/BOJ_1620.cpp
@@ -1,34 +1,33 @@
 #include<iostream>
 #include<unordered_map>
-#include<vector>
 #include<string>
 using namespace std;
 
 int main(){
     ios_base::sync_with_stdio(false), cin.tie(NULL);
-    
+
     int n, m;
     cin>>n>>m;
 
     unordered_map<string, int> name;
     unordered_map<int, string> number;
-
     string str;
+
     for(int i = 1; i <= n; i++){
         cin>>str;
-        name[str] = i;
-        number[i] = str;
+        name[str]=i;
+        number[i]=str;
     }
 
     string query;
     for(int i = 0; i < m; i++){
         cin>>query;
-        if(query[0] - '0' >= 0 && query[0] - '0' <= 9){
-            int tmp = stoi(query);
-            cout<<number[tmp]<<"\n";
+        if(query[0] >= '0' && query[0] <= '9'){
+            cout<<number[stoi(query)]<<"\n";
         }else{
             cout<<name[query]<<"\n";
         }
     }
+    
     return 0;
 }
