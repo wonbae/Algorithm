d9363714a8ba308c14825f79e4c8a7dd04299d04
wonbae kim <k1bae2301@gmail.com>
2022-09-22 09:21:17 +0900

    오큰수, stack

---
+++ BOJ/BOJ_17298/BOJ_17298.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n, a[1000004], ret[1000004];
+stack<int> st;
+
+int main(){
+    ios_base::sync_with_stdio(0), cin.tie(NULL);
+    
+    cin>>n;
+    memset(ret, -1, sizeof(ret));
+    for(int i = 0; i < n; i++){
+        cin>>a[i];
+        while(st.size() && a[st.top()] < a[i]){
+            ret[st.top()] = a[i];
+            st.pop();
+        }
+        st.push(i);
+    }
+
+    for(int i = 0; i < n; i++){
+        cout<<ret[i]<<" ";
+    }
+
+    return 0;
+}
