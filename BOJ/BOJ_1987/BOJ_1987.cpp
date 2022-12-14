fe9017b5eba7b0fc1b15c3551fa636e46ce96a0b
wonbae kim <k1bae2301@gmail.com>
2022-12-14 18:48:25 +0900

    알파벳

--- BOJ/BOJ_1987/BOJ_1987.cpp
+++ BOJ/BOJ_1987/BOJ_1987.cpp
@@ -8,18 +8,16 @@ int R, C, cnt = 0, ans = 0;
 const int dx[] = {-1, 0, 1, 0};
 const int dy[] = {0, 1, 0, -1};
 
-void dfs(int r, int c, int cnt){
+void dfs(int r, int c, int cnt, int num){
     ans = max(cnt, ans);
 
     for(int i = 0; i < 4; i++){
         int nr = r + dx[i];
         int nc = c + dy[i];
         if(nr < 0 || nr >= R || nc < 0 || nc >= C) continue;
-        int next = bord[nr][nc];
-        if(check[next] == 0){
-            check[next] = 1;
-            dfs(nr,nc,cnt+1);
-            check[next] = 0; 
+        int next = (1<<bord[nr][nc]);
+        if((num & next) == 0){
+            dfs(nr, nc, cnt+1, num|next);
         }
     }
     return ;
@@ -38,9 +36,56 @@ int main(){
     }
 
     check[bord[0][0]]=true;
-    dfs(0,0,1);
+    dfs(0,0,1, (1<<bord[0][0]));
     
     cout<<ans<<"\n";
 
     return 0;
 }
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int bord[21][21];
+// bool check[26]={false,};
+// int R, C, cnt = 0, ans = 0;
+
+// const int dx[] = {-1, 0, 1, 0};
+// const int dy[] = {0, 1, 0, -1};
+
+// void dfs(int r, int c, int cnt){
+//     ans = max(cnt, ans);
+
+//     for(int i = 0; i < 4; i++){
+//         int nr = r + dx[i];
+//         int nc = c + dy[i];
+//         if(nr < 0 || nr >= R || nc < 0 || nc >= C) continue;
+//         int next = bord[nr][nc];
+//         if(check[next] == 0){
+//             check[next] = 1;
+//             dfs(nr,nc,cnt+1);
+//             check[next] = 0; 
+//         }
+//     }
+//     return ;
+// }
+
+// int main(){
+//     ios_base::sync_with_stdio(false),cin.tie(NULL);
+//     cin>>R>>C;
+//     string str;
+
+//     for(int i = 0; i < R; i++){
+//         cin>>str;
+//         for(int j = 0; j < str.size(); j++){
+//             bord[i][j] = str[j]-'A';
+//         }
+//     }
+
+//     check[bord[0][0]]=true;
+//     dfs(0,0,1);
+    
+//     cout<<ans<<"\n";
+
+//     return 0;
+// }
