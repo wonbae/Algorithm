f34f58a601c2eed2c32cdb08965cc3559c60dd9a
wonbae kim <k1bae2301@gmail.com>
2023-01-05 23:56:22 +0900

    회의실 배정, greedy

--- BOJ/BOJ_1931/BOJ_1931.cpp
+++ BOJ/BOJ_1931/BOJ_1931.cpp
@@ -1,37 +1,31 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include<bits/stdc++.h>
+#define fastio ios_base::sync_with_stdio(flase) cin.tie(NULL), cout.tie(NULL);
 using namespace std;
 
-bool cmp(pair<int, int> a, pair<int, int> b)
-{
-    if(a.second < b.second) return true;
-    else if(a.second > b.second) return false;
-    if(a.first < b.first) return true;
-    else return false;
+bool cmp(pair<int, int> a, pair<int, int> b){
+    if(a.second == b.second) return a.first < b.first;
+    return a.second < b.second;
 }
 
 int main(){
-    int N, endtime = -2147000000, cnt = 0;
-    cin>>N;
+    int n, ans = 0, s, e;
     vector<pair<int, int> > v;
+    cin>>n;
 
-    for(int i = 0; i < N; i++){
-        int s, e;
+    for(int i = 0; i < n; i++){
         cin>>s>>e;
-        v.push_back(make_pair(s,e));
+        v.push_back({s,e});
     }
-
+    
     sort(v.begin(), v.end(), cmp);
 
+    int tmp=0;
     for(int i = 0; i < v.size(); i++){
-        if(v[i].first >= endtime){
-            endtime = v[i].second;
-            cnt++;
+        if(tmp <= v[i].first){
+            tmp = v[i].second;
+            ans++;
         }
     }
-
-    cout<<cnt<<"\n";
-
+    cout<<ans;
     return 0;
 }
