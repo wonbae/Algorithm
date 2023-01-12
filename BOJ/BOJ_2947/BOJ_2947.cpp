54fe7bf21f0125ddfd2701a8aac68256ba1ef73c
wonbae kim <k1bae2301@gmail.com>
2023-01-12 12:18:59 +0900

    나무 조각

--- BOJ/BOJ_2947/BOJ_2947.cpp
+++ BOJ/BOJ_2947/BOJ_2947.cpp
@@ -7,19 +7,34 @@ int main(){
     for(int i = 0; i < 5; i++){
         cin>>arr[i];
     }
-    int cnt = 5;
-    int i = 0;
+
     while(1){
-        if(i>=4) i = 0;
-        if(arr[i] > arr[i+1]){
-            int tmp = arr[i+1];
-            arr[i+1] = arr[i];
-            arr[i] = tmp;
-            for(int j = 0; j < 5; j++){
-                cout<<arr[j]<<" ";
-            }cout<<"\n";
+        for(int i = 0; i < 4; i++){
+            bool tik = false;
+            if(arr[i] > arr[i+1]){
+                int tmp = arr[i+1];
+                arr[i+1] = arr[i];
+                arr[i] = tmp;
+                tik = true;
+            }
+            if(tik){
+                for(int j = 0; j < 5; j++){
+                    cout<<arr[j]<<" ";
+                }cout<<"\n";
+            }
         }
-        i++;
+
+        bool flag = false;
+        for(int i = 0; i < 4; i++){
+            if(arr[i] < arr[i+1]) continue;
+            else{
+                flag = true;
+                break;
+            }
+        }
+        
+        if(!flag) break;
+
     }
 
     return 0;
