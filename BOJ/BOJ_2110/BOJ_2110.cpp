60329141052bc6316d0ea46a14d2ebea976808d4
wonbae kim <k1bae2301@gmail.com>
2023-02-22 09:41:26 +0900

    공유기 설치, 이분탐색

--- BOJ/BOJ_2110/BOJ_2110.cpp
+++ BOJ/BOJ_2110/BOJ_2110.cpp
@@ -47,7 +47,7 @@
 #include <stdio.h>
 #include <algorithm>
 using namespace std;
-int N, C;
+int N, C, ans;
 int x[200005];
  
 bool isPossibleDist(int dist) { // 공유기 사이의 거리를 dist로 하는 것이 가능한지
@@ -66,14 +66,16 @@ int main(void) {
     for (int i = 0; i < N; i++) 
         scanf("%d", &x[i]);
     sort(x, x + N);
-    int lo = 1;
+    int lo = 0;
     int hi = 1000000000; // lo~hi-1 안에 답이 존재
-    while (hi > lo + 1) { // 이분탐색
+    while (lo <= hi) { // 이분탐색
         int mid = (lo + hi) / 2;
-        if (isPossibleDist(mid)) // 공유기 간 거리가 mid일 때 C개 이상 설치 가능하다면
-            lo = mid; // 거리를 늘려볼 것임
+        if (isPossibleDist(mid)){ // 공유기 간 거리가 mid일 때 C개 이상 설치 가능하다면
+            lo = mid + 1; // 거리를 늘려볼 것임
+            ans = mid;
+        }
         else // 공유기 간 거리가 mid일 때 C개 이상 설치할 수 없다면
-            hi = mid; // 거리를 줄임
+            hi = mid - 1; // 거리를 줄임
     }
-    printf("%d", lo);
+    printf("%d", ans);
 }
