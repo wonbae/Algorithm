154c4cabb7001dc9e2e0dcfa8172d378d74d1d40
wonbae kim <k1bae2301@gmail.com>
2023-08-09 23:20:20 +0900

    가장 긴 증가하는 부분 수열 5

--- BOJ/BOJ_14003/BOJ_14003.cpp
+++ BOJ/BOJ_14003/BOJ_14003.cpp
@@ -1,26 +1,27 @@
 #include<bits/stdc++.h>
 #define fastio ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
-#define INF 2147483648
+#define INF 1e9+4
 typedef std::pair<int, int> pii;
 
 using namespace std;
 
 int n, num, cnt;
 vector<pii> tmp;
-vector<int> v;
-stack<int> st;
+vector<int> v(1000004, INF);
+// stack<int> st;
+vector<int> ans;
 
 int main(){
     fastio;
     cin>>n;
-    v.resize(n,0);
+    v.resize(n+4, INF);
 
     for(int i = 0; i < n; i++){
         cin>>num;
         
         auto it = lower_bound(v.begin(), v.begin()+cnt, num);
         int idx = (int)(lower_bound(v.begin(), v.begin()+cnt, num) - v.begin());
-        if(*it == 0){
+        if(*it == INF){
             cnt++;
         }
         *it = num;
@@ -29,51 +30,32 @@ int main(){
     }
 
     cout<<cnt<<"\n";
+    
+    reverse(tmp.begin(), tmp.end());
 
-    for(int i = tmp.size()-1; i >= 0; i--){
-        if(tmp[i].first == cnt-1){
-            st.push(tmp[i].second);
-            cnt--;
+    for(int i = 0; i < tmp.size(); i++){
+        if(tmp[i].first == cnt - 1){
+            ans.push_back(tmp[i].second);
+            cnt -= 1;
         }
     }
-    
-    while(!st.empty()){
-        cout<<st.top()<<" ";
-        st.pop();
+
+    reverse(ans.begin(), ans.end());
+
+    for(int i = 0; i < ans.size(); i++){
+        cout<<ans[i]<<" ";
     }
+
+    // for(int i = tmp.size()-1; i >= 0; i--){
+    //     if(tmp[i].first == cnt-1){
+    //         st.push(tmp[i].second);
+    //         cnt--;
+    //     }
+    // }
+    
+    // while(!st.empty()){
+    //     cout<<st.top()<<" ";
+    //     st.pop();
+    // }
     return 0;
 }
-
-
-
-// #include <cstdio>
-// #include <algorithm>
-// #include <stack> 
-// using namespace std; 
-// int n, lis[1000004], len, num;
-// pair<int, int> ans[1000004];
-// stack<int> stk; 
-// int main() {
-// 	scanf("%d", &n);
-// 	for (int i = 0; i < n; i++){
-//         scanf("%d", &num);
-//         auto lowerPos = lower_bound(lis, lis + len, num);
-//         int _pos = (int)(lower_bound(lis, lis + len, num) - lis);
-//         if(*lowerPos == 0) len++;
-//         *lowerPos = num;  
-//         ans[i].first = _pos;
-//         ans[i].second = num; 
-//         printf("%d , %d\n",_pos, num);
-//     }
-// 	printf("%d\n", len);
-//     for(int i = 0; i < n; i++)printf("%d %d\n", ans[i].first, ans[i].second);
-//     for(int i = n - 1; i >= 0; i--){
-//         if(ans[i].first == len - 1){
-//             stk.push(ans[i].second); len--;
-//         }
-//     }
-//     while(stk.size()){
-//         printf("%d ", stk.top());stk.pop();
-//     } 
-// 	return 0;
-// }
