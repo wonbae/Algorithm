0dd39b059393e0fa0b1c2bbdc2d84e05a422d30b
wonbae kim <k1bae2301@gmail.com>
2023-08-20 23:43:58 +0900

    강의실 배정, pq

---
+++ BOJ/BOJ_11000/BOJ_11000.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+int main(){
+    fastio;
+
+    int n, s, e, room = 1;
+    cin>>n;
+
+    vector<pair<int, int>> v;
+    priority_queue<int, vector<int>, greater<int>> pq;
+
+    for(int i = 0; i < n; i++){
+        cin>>s>>e;
+        v.push_back({s, e});
+    }
+
+    sort(v.begin(), v.end());
+
+    
+    pq.push(v[0].second);
+
+    for(int i = 1; i < n; i++){
+        if(pq.top() <= v[i].first)
+            pq.pop();
+        pq.push(v[i].second);
+    }
+
+    cout<<(int)pq.size();
+    return 0;
+}
