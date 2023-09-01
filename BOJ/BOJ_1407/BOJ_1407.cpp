49547edf302ac103e007e095f05ffc4c99826da8
wonbae kim <k1bae2301@gmail.com>
2023-09-01 19:29:04 +0900

    2로 몇 번 나누어질까, math

--- BOJ/BOJ_1407/BOJ_1407.cpp
+++ BOJ/BOJ_1407/BOJ_1407.cpp
@@ -3,20 +3,40 @@
 using namespace std;
 typedef long long ll;
 
-ll fn(ll num){
-    ll n = num;
-    for(ll i = 2; i <=num; i*=2){
-        n +=(num/i)*(i/2);
-        cout<<n<<",";
-    }cout<<"\n";
-    return n;
+// ll func(ll x){
+//     ll y = 0, res = 0;
+
+//     for(ll i = 1; x > 0; i *= 2){
+//         if(x & 1){
+//             y = x / 2 + 1;
+//         }else{
+//             y = x / 2;
+//         }
+        
+//         res += (y * i);
+        
+//         x -= y;
+//     }
+
+//     return res;
+// }
+
+ll func(ll x){
+    ll r = x;
+    for(ll i = 2; i <= x; i *= 2){
+        r += (x/i) * (i/2);
+    }
+    return r;
 }
 
-int main(){
-    ios_base::sync_with_stdio(false), cin.tie(NULL);
-    ll a, b;
 
+int main(){
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+    
+    ll a, b, cnt = 0, ans = 0;
     cin>>a>>b;
-    cout<<fn(b) - fn(a-1);
+
+    cout<<(func(b) - func(a-1))<<"\n";
+
     return 0;
 }
