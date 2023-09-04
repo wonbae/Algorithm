e368ea6c7d114424a32a359ff09b2d1c1bd7fbfb
wonbae kim <k1bae2301@gmail.com>
2023-09-04 10:07:33 +0900

    연속합 2, DP, Kadane algorithm

--- BOJ/BOJ_13398/BOJ_13398.cpp
+++ BOJ/BOJ_13398/BOJ_13398.cpp
@@ -16,12 +16,13 @@ int main() {
     vector<vector<long long>> dp(n, vector<long long>(2, 0));
 
     dp[0][0] = v[0]; 
+    dp[0][1] = v[0];
     long long ans = v[0];
 
     for (int i = 1; i < n; i++) {
         dp[i][0] = max((long long)0, dp[i-1][0]) + v[i];    // 제거하지 않고 전체 중에 가장 큰 연속된 부분수열
         dp[i][1] = max(dp[i-1][0], dp[i-1][1] + v[i]);
-
+        
         ans = max(dp[i][0], ans);
         ans = max(dp[i][1], ans);
     }
