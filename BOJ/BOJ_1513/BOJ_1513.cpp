833937ceb7fe42bb767999a889d7c63c53ba3650
wonbae kim <k1bae2301@gmail.com>
2023-09-05 22:14:56 +0900

    경로 찾기, dp, 4차원, 개어려움

--- BOJ/BOJ_1513/BOJ_1513.cpp
+++ BOJ/BOJ_1513/BOJ_1513.cpp
@@ -1,35 +1,54 @@
 #include<bits/stdc++.h>
 using namespace std;
-int INF = 987654321;
-int dp[100001];
 
-int main(){
-    int n;
-    cin>>n;
+const int MOD = 1000007;
+int dp[51][51][51][51];
+int v[51][51];
 
-    dp[0] = 0;
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(NULL), cout.tie(NULL);
+    
+    int n, m, c;
+    cin>>n>>m>>c;
 
-    vector<int> coins;
-    coins.push_back(2);
-    coins.push_back(5);
-
-    // Initialize dp array
-    for (int i = 1; i <= n; i++) {
-        dp[i] = INF; // Initialize with a large value
+    int a, b;
+    for(int i = 1; i <= c; i++){
+        cin>>a>>b;
+        v[a][b] = i;
     }
-    dp[0] = 0;
 
-    for(int i = 0; i <= n; i++){
-        for(auto coin : coins){
-            if(coin <= i)
-               dp[i] = min(dp[i], dp[i - coin] + 1);
+    if(v[1][1] != 0){
+        dp[1][1][1][v[1][1]] = 1;
+    }else{
+        dp[1][1][0][0] = 1;
+    }
+
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= m; j++){
+            int game = v[i][j];
+    
+            if(game != 0){
+                for(int num = 0; num <= game; num++){
+                    for(int visit = 0; visit <= num; visit++){
+                        dp[i][j][visit+1][game] += (dp[i-1][j][visit][num] + dp[i][j-1][visit][num]) % MOD;
+                    }
+                }
+            }else{
+                for(int num = 0; num <= c; num++){
+                    for(int visit = 0; visit <= num; visit++){
+                        dp[i][j][visit][num] += (dp[i-1][j][visit][num] + dp[i][j-1][visit][num]) % MOD;
+                    }
+                }
+            }
         }
     }
 
-    if(dp[n] == INF) {
-        cout << -1;
-    } else {
-        cout << dp[n];
+    for(int i = 0; i <= c; i++){
+        int tmp = 0;
+        for(int j = 0; j <= c; j++){
+            tmp += dp[n][m][i][j];
+        }
+        cout<<tmp%MOD<<" ";
     }
 
     return 0;
