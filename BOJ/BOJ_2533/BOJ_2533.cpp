f6c6a2cf012e627c3398697114f513f2360d2f63
wonbae kim <k1bae2301@gmail.com>
2023-09-15 17:40:36 +0900

    사회망 서비스(SNS) , DP, tree, ta

--- BOJ/BOJ_2533/BOJ_2533.cpp
+++ BOJ/BOJ_2533/BOJ_2533.cpp
@@ -3,21 +3,21 @@ using namespace std;
 #define fastio ios_base::sync_with_stdio(0), cin.tie(NULL), cout.tie(NULL);
 
 vector<int> adj[1000001];
+int dp[1000001][2];
 int visited[1000001];
-int dp[1000001][2]; // 0:early, 1:normal
 
 void solve(int node){
-
-    dp[node][0] = 1;
-
     visited[node] = true;
+
+    dp[node][0] = 1; // 0:early adopter
 
     for(auto &child : adj[node]){
         if(visited[child]) continue;
+
         solve(child);
 
         dp[node][1] += dp[child][0];
-        dp[node][0] += min(dp[child][0], dp[child][1]);
+        dp[node][0] += min(dp[child][1], dp[child][0]);
     }
 
     return;
@@ -25,12 +25,11 @@ void solve(int node){
 
 int main(){
     fastio;
-    
-    int n;
+
+    int n, u, v;
     cin>>n;
 
-    int u, v;
-    for(int i = 0; i < n-1; i++){
+    for(int i = 1; i < n; i++){
         cin>>u>>v;
         adj[u].push_back(v);
         adj[v].push_back(u);
@@ -39,6 +38,4 @@ int main(){
     solve(1);
 
     cout<<min(dp[1][0], dp[1][1])<<"\n";
-
-    return 0;
 }
